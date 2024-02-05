52c633e2d2739b26efb4ac9925ca4c4e41867931
alberto <alberto.taddeucci@gmail.com>
2024-02-05 14:23:22 +0100

    snack #2

---
+++ Snack2/script.js
@@ -0,0 +1,29 @@
+/*
+Genera un numero casuale tra 1 e 100
+ e poi chiedi all'utente di indovinare il numero.
+  Il programma poi deve far comparire un messaggio "il numero è troppo alto"
+   oppure "il numero è troppo basso" 
+   nel momento in cui il numero detto dall'utente 
+   non è perfettamente uguale al numero estratto in modo casuale.
+Quando l'utente trova il numero, fateglielo sapere!
+*/
+
+const randomNum = Math.round(Math.random() * 100 + 1);
+
+
+let userNum;
+do {
+    
+    userNum = prompt("Inserisci un numero");
+
+    if (isNaN(userNum)||(userNum === "")){
+        alert("Devi inseirre un numero!")
+    } else if (userNum>randomNum){
+        alert("Ritenta: Numero troppo alto!");
+    } else if (userNum<randomNum) {
+        alert("Ritenta: Numero troppo basso!");
+    }
+    
+} while (userNum != randomNum);
+
+document.querySelector("#result").innerHTML = "Hai indovinato!"
